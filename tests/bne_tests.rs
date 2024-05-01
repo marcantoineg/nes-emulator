a2ff6998232a0ec0db20afaf1262900f12200f00
Marc-Antoine Giguère <magiguere@mirego.com>
2024-05-01 20:29:58 +0000

    add operation - BNE

--- tests/bne_tests.rs
+++ tests/bne_tests.rs
@@ -5,11 +5,11 @@ mod common;
 use common::{assert_no_flags, assert_flags};
 
 #[test]
-fn test_0x90_bcc_relative_branches_forward_correctly() {
+fn test_0xd0_bne_relative_branches_forward_correctly() {
     let mut cpu = CPU::new();
     
     cpu.load_and_run_without_reset(vec![
-        /*BCC+2*/ 0x90, 0x02,
+        /*BNE+2*/ 0xD0, 0x02,
         /*LDA*/ 0xA9, 0x02,
         0x00
     ]);
@@ -19,13 +19,13 @@ fn test_0x90_bcc_relative_branches_forward_correctly() {
 }
 
 #[test]
-fn test_0x90_bcc_relative_branches_backward_correctly() {
+fn test_0xd0_bne_relative_branches_backward_correctly() {
     let mut cpu = CPU::new();
     
     cpu.load_and_run_without_reset(vec![
         /*LDA*/ 0xA9, 0xFE,
         /*ADC*/ 0x69, 0x01,
-        /*BCC-4*/ 0x90, 0x84,
+        /*BNE-4*/ 0xD0, 0x84,
         0x00
     ]);
 
@@ -34,28 +34,28 @@ fn test_0x90_bcc_relative_branches_backward_correctly() {
 }
 
 #[test]
-fn test_0x90_bcc_relative_ignores_branching_when_offset_is_zero() {
+fn test_0xd0_bne_relative_ignores_branching_when_offset_is_zero() {
     let mut cpu = CPU::new();
 
     cpu.load_and_run_without_reset(vec![
-        /*LDA*/ 0xA9, 0x01,
+        /*LDA*/ 0xA9, 0x00,
+        /*BNE+-0*/ 0xD0, 0x00,
         /*ADC*/ 0x69, 0x01,
-        /*BCC+-0*/ 0x90, 0x00,
         0x00
     ]);
 
-    assert_eq!(cpu.register_a, 0x02);
+    assert_eq!(cpu.register_a, 0x01);
     assert_no_flags(&cpu);
 }
 
 #[test]
-fn test_0x90_bcc_relative_ignores_branching_when_condition_is_not_met() {
+fn test_0xd0_bne_relative_ignores_branching_when_condition_is_not_met() {
     let mut cpu = CPU::new();
 
     cpu.load_and_run_without_reset(vec![
         /*LDA*/ 0xA9, 0xFF,
         /*ADC*/ 0x69, 0x01,
-        /*BCC-4*/ 0x90, 0x84,
+        /*BNE-4*/ 0xD0, 0x84,
         0x00
     ]);
 
